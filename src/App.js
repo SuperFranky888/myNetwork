7d6b8be4bd3185a74b0245ec0ce075c7ba0577f1
SuperFranky888 <super_franky88@mail.ru>
2021-12-01 23:52:48 +0300

    added Router ( ROuting)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes,  BrowserRouter} from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
@@ -8,16 +9,22 @@ import Profile from './components/Profile/Profile';
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-     
+
       <div className="app-wrapper-content">
-          <Profile />
-          <Dialogs />
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dialogs" element={<Dialogs />} />
+        </Routes>
       </div>
-   
-    </div>);
+
+    </div>;
+      
+    </BrowserRouter>
+  )
 }
 
 export default App;
